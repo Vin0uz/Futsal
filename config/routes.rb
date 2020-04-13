e7e41eea839780158c0e9c628044c0898da6ad37
Kevin Vinhas <kevinvinhas@gmail.com>
2020-04-13 22:58:17 +0200

    Setting up default administrate. :warning: it uses sprockets we need to move out from this if possible - Closes #2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :goals
+      resources :players
+      resources :teams
+      resources :games
+
+      root to: "goals#index"
+    end
   root "home#show"
 end
