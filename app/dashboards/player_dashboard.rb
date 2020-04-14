3204786b7b324ecb511cc69406e7917959115ee6
Kevin Vinhas <kevinvinhas@gmail.com>
2020-04-14 22:18:07 +0200

    Fixing forgotten style mistakes

--- app/dashboards/player_dashboard.rb
+++ app/dashboards/player_dashboard.rb
@@ -22,33 +22,33 @@ class PlayerDashboard < Administrate::BaseDashboard
   #
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
-  COLLECTION_ATTRIBUTES = %i[
-  team
-  goals
-  assists
-  id
+  COLLECTION_ATTRIBUTES = [
+    "team",
+    "goals",
+    "assists",
+    "id",
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
-  SHOW_PAGE_ATTRIBUTES = %i[
-  team
-  goals
-  assists
-  id
-  name
-  created_at
-  updated_at
+  SHOW_PAGE_ATTRIBUTES = [
+    "team",
+    "goals",
+    "assists",
+    "id",
+    "name",
+    "created_at",
+    "updated_at",
   ].freeze
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
-  FORM_ATTRIBUTES = %i[
-  team
-  goals
-  assists
-  name
+  FORM_ATTRIBUTES = [
+    "team",
+    "goals",
+    "assists",
+    "name",
   ].freeze
 
   # COLLECTION_FILTERS
