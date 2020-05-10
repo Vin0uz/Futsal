a1efd1dedef37970bdd85c75021c33ed9c39d81b
Kevin Vinhas <kevinvinhas@gmail.com>
2020-05-10 22:04:46 +0200

    Creating two first charts, using stimulus js datamaps and connect to create and dislay them
    This would need a cleaning in the controller, holding way to much logic, should probably be extracted in a service later
    I just display two charts, we need to add style on the page to make it look somehow cool
    
    Also important : stop using home route and start routing properly..

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,42 @@
 class WelcomeController < ApplicationController
-  def home; end
+  def home
+    games = Game.all.pluck(:matchweek).uniq
+    action_types = Goal.all.pluck(:action_type).uniq
+    teams = Team.all.pluck(:acronym).uniq
+    @chart1 = build_chart_data(labels: games, attributes: action_types, type: "goals_action_matchweek")
+    @chart2 = build_chart_data(labels: teams, attributes: action_types, type: "goals_action_team")
+  end
+
+  private
+
+  def build_chart_data(labels:, attributes:, type:)
+    chart_data = {labels: labels, attributes: attributes, data: nil}
+
+    if type == "goals_action_matchweek"
+      query_results = Goal.joins(:game).group("games.matchweek", "goals.action_type").count
+    elsif type == "goals_action_team"
+      query_results = Goal.joins(scorer: :team).group("teams.acronym", "goals.action_type").count
+    end
+
+    formatted_response = transform_query_group(query_results)
+
+    chart_data[:data] = []
+    attributes.each do |attribute|
+      attribute_arr = []
+      labels.each do |label|
+        attribute_arr << (formatted_response.dig(label, attribute) || 0)
+      end
+      chart_data[:data] << attribute_arr
+    end
+    chart_data
+  end
+
+  def transform_query_group(results)
+    formatted_result = {}
+    results.each do |(key, key2), quantity|
+      formatted_result[key] ||= {}
+      formatted_result[key][key2] = quantity
+    end
+    formatted_result
+  end
 end
--- app/javascript/controllers/home_controller.js
+++ app/javascript/controllers/home_controller.js
@@ -3,34 +3,17 @@ import { Controller } from "stimulus"
 import Chart from "chart.js"
 
 export default class extends Controller {
-  static targets = [ "name", "output" ]
+  static targets = [ "labels", "data", "datasetLabels" ]
 
   connect(){
+    var ctx = document.getElementById(this.data.get("canvasId")).getContext('2d');
+    var labels = this.labels
+    var length = labels.length
     var myChart = new Chart(ctx, {
-      type: 'bar',
+      type: "bar",
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
+        labels: this.labels,
+        datasets: this.datasets
       },
       options: {
         scales: {
@@ -43,4 +26,77 @@ export default class extends Controller {
       }
     });
   }
+
+  get labels() {
+    return JSON.parse(this.labelsTarget.dataset.value)
+  }
+
+  get datasets() {
+    var datasets = []
+    var hash = {}
+    var data = this.dataChart
+    const COLORS = this.colors
+    const BORDER_COLORS = this.borderColors
+    this.dataset_labels.forEach(function (action_type, j){
+      hash = {}
+      hash.label = action_type
+      hash.data = data[j]
+      hash.backgroundColor = COLORS[j]
+      hash.borderColor = BORDER_COLORS[j]
+      hash.borderWidth = 1
+      datasets.push(hash)
+    });
+
+    return datasets
+  }
+
+  get dataset_labels(){
+    return JSON.parse(this.datasetLabelsTarget.dataset.value)
+  }
+
+  get dataChart(){
+    return JSON.parse(this.dataTarget.dataset.value)
+  }
+
+  colorArray(color, count) {
+    var bgColors = [];
+    for (var i = 0; i < count; i++) {
+      bgColors.push(color);
+    }
+    return bgColors
+  }
+
+  get colors(){
+    return ["rgba(255, 99, 132, 0.5)",
+      "rgba(255, 159, 64, 0.5)",
+      "rgba(255, 205, 86, 0.5)",
+      "rgba(75, 192, 192, 0.5)",
+      "rgba(54, 162, 235, 0.5)",
+      "rgba(153, 102, 255, 0.5)"
+    ]
+  }
+
+  get borderColors(){
+    return ["rgba(255, 99, 132, 1)",
+      "rgba(255, 159, 64, 1)",
+      "rgba(255, 205, 86, 1)",
+      "rgba(75, 192, 192, 1)",
+      "rgba(54, 162, 235, 1)",
+      "rgba(153, 102, 255, 1)"
+    ]
+  }
+  //[{
+  //        label: "Autres",
+  //       data: [12, 19, 3, 5, 2, 3],
+  //        backgroundColor: this.colorArray("rgba(255, 99, 132, 0.2)", length),
+  //        borderColor: this.colorArray("rgba(255, 99, 132, 1)", length),
+  //        borderWidth: 1
+  //      },
+  //      {
+  //        label: "Strategie",
+  //        data: [1, 3, 7, 1, 2, 3],
+  //        backgroundColor: this.colorArray("rgba(255, 159, 64, 0.2)", length),
+  //        borderColor: this.colorArray("rgba(255, 159, 64, 1)", length),
+  //        borderWidth: 1
+  //      }]
 }
