7fc8f8895f53ddaf93bb574392a37982aad63e1b
Kevin Vinhas <kevinvinhas@gmail.com>
2020-05-17 14:13:19 +0200

    Extracting a ChartController that will be parent of all small chart controller
    It allows have simple controllers sharing all the same base, in ChartController, making it easier to add new ones.
    I wanted to do this before touching Chart2D so that I can test/find a nice way to apply this concept before touching the mess of Chart2D :)

---
+++ app/javascript/controllers/charts/action_type_goals_controller.js
@@ -0,0 +1,17 @@
+import  ChartController from "../chart_controller.js"
+export default class extends ChartController {
+
+  connect(){
+    super.connect()
+    this.initChart(this.chartType)
+    this.refreshChart()
+  }
+
+  get apiUrl(){
+    return "/api/action_type_goals"
+  }
+
+  get chartType(){
+    return "polarArea"
+  }
+}
