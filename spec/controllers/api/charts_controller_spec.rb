5aeb26faa246ddb39035ecc89fc0156ec00f8370
Kevin Vinhas <kevinvinhas@gmail.com>
2020-05-17 18:15:35 +0200

    Adding specs of recently added entries of charts api

--- spec/controllers/api/charts_controller_spec.rb
+++ spec/controllers/api/charts_controller_spec.rb
@@ -27,4 +27,34 @@ describe Api::ChartsController do
       expect(parsed_body["data"]).to eq("ZZ" => 1)
     end
   end
+
+  describe "#goal_action_type_teams" do
+    it "returns labels and data as hash of arrays" do
+      team1 = create(:team, acronym: "ZZ")
+      team2 = create(:team, acronym: "team2")
+      player = create(:player, team: team1)
+      create(:goal, action_type: "Nice", scorer: player, game: create(:game, home_team: team1, away_team: team2))
+
+      get(:goal_action_type_teams)
+
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body["labels"]).to eq(["ZZ", "team2"])
+      expect(parsed_body["data"]).to eq("Nice" => [1, 0])
+    end
+  end
+
+  describe "#goal_action_type_matchweeks" do
+    it "returns labels and data as hash of arrays" do
+      team1 = create(:team, acronym: "ZZ")
+      team2 = create(:team, acronym: "team2")
+      player = create(:player, team: team1)
+      create(:goal, action_type: "Nice", scorer: player, game: create(:game, matchweek: "Day1", home_team: team1, away_team: team2))
+
+      get(:goal_action_type_matchweeks)
+
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body["labels"]).to eq(["Day1"])
+      expect(parsed_body["data"]).to eq("Nice" => [1])
+    end
+  end
 end
