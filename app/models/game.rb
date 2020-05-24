153b33eadb5f3c4fc8895da511df31d3b9bbe6c2
Kevin Vinhas <kevinvinhas@gmail.com>
2020-05-24 16:52:37 +0200

    Adding a default scope ordering games by match_week
    Also adding it in the model spec so that we remember that there is this default scope existing
    Cleaned out the matchweek elements, can remove it in next commit.
    
    Also, removed useless `.all` on some queries that was fetching results for nothing too early.

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,6 @@ class Game < ApplicationRecord
   has_many :goals
   has_many :team_games
   has_many :teams, through: :team_games
+
+  default_scope { order(match_week: :asc) }
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -4,4 +4,8 @@ describe Game do
   it { should have_many(:goals) }
   it { should have_many(:team_games) }
   it { should have_many(:teams).through(:team_games) }
+
+  it "applies a default scope to order by match_weeks" do
+    expect(Game.all.to_sql).to eq Game.all.order(match_week: :asc).to_sql
+  end
 end
