b834df6c87674fc4d84516f475c924d4b3563868
Kevin Vinhas <kevinvinhas@gmail.com>
2020-08-16 15:58:28 +0200

    Add main stats on dashboard: ranking, last 5 games trend and last game score

---
+++ app/helpers/dashboard_helper.rb
@@ -0,0 +1,20 @@
+module DashboardHelper
+  def team_ranking(team:, ranking:)
+    ranking.keys.index(team.acronym)
+  end
+
+  def last_results(team:)
+    team.games.last(5)
+  end
+
+  def last_game_scoreboard(team:)
+    game = team.games.where("date < ?", DateTime.current).order(match_week: :desc).first
+    if game.present?
+      team_game_home = game.team_games.find_by(home: true)
+      team_game_away = game.team_games.find_by(home: false)
+      "#{team_game_home.team.acronym} #{team_game_home.goals_scored} - #{team_game_away.goals_scored} #{team_game_away.team.acronym}"
+    else
+      " - "
+    end
+  end
+end
--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,30 @@ class Team < ApplicationRecord
   has_many :players
   has_many :team_games
   has_many :games, through: :team_games
+
+  def total_points
+    (team_games.where(result: "win").count * 3) + (team_games.where(result: "draw").count)
+  end
+
+  def total_goals_for
+    team_games.sum(:goals_scored)
+  end
+
+  def total_goals_against
+    team_games.sum(:goals_conceded)
+  end
+
+  def goal_average
+    total_goals_for - total_goals_against
+  end
+
+  def ranking_details
+    details = {}
+    details[:games] = games.count
+    details[:points] = total_points
+    details[:goals_for] = total_goals_for
+    details[:goals_against] = total_goals_against
+    details[:goal_average] = goal_average
+    details
+  end
 end
