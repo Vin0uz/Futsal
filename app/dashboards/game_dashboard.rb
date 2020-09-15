eee661df516029b96b8f62cdfbc2f898e8b3c9e4
Kevin Vinhas <kevinvinhas@gmail.com>
2020-09-15 14:21:20 +0200

    Improving game displaying in admin collections
    Also adding youtube id in form for goal creation :)

--- app/dashboards/game_dashboard.rb
+++ app/dashboards/game_dashboard.rb
@@ -4,24 +4,23 @@ class GameDashboard < Administrate::BaseDashboard
   ATTRIBUTE_TYPES = {
     goals: Field::HasMany,
     teams: Field::HasMany,
-    id: Field::String,
-    match_week: Field::Number,
+    match_week: Field::Number.with_options(searchable: true),
     date: Field::DateTime,
+    display_game: Field::String.with_options(searchable: false),
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
   }.freeze
 
   COLLECTION_ATTRIBUTES = [
-    :match_week,
-    :teams,
+    :display_game,
     :goals,
   ].freeze
 
   SHOW_PAGE_ATTRIBUTES = [
-    :goals,
-    :id,
-    :match_week,
+    :display_game,
+    :teams,
     :date,
+    :goals,
     :created_at,
     :updated_at,
   ].freeze
@@ -32,5 +31,9 @@ class GameDashboard < Administrate::BaseDashboard
     :date,
   ].freeze
 
-  COLLECTION_FILTERS = {}.freeze
+  COLLECTION_FILTERS = {}.frezee
+
+  def display_resource(game)
+    "J#{game.match_week}: #{game.teams.first.acronym} - #{game.teams.second.acronym}"
+  end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -4,4 +4,8 @@ class Game < ApplicationRecord
   has_many :teams, through: :team_games
 
   default_scope { order(match_week: :asc) }
+
+  def display_game
+    "J#{match_week}: #{teams.first.acronym} - #{teams.second.acronym}"
+  end
 end
