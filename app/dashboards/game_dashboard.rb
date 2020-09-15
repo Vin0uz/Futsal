9fbd9a42735d882085c6f1f47e526dbd3d602cea
Kevin Vinhas <kevinvinhas@gmail.com>
2020-09-15 11:51:29 +0200

    Bumping gems and symbolizing/cleaning dashboards of admin

--- app/dashboards/game_dashboard.rb
+++ app/dashboards/game_dashboard.rb
@@ -1,67 +1,36 @@
 require "administrate/base_dashboard"
 
 class GameDashboard < Administrate::BaseDashboard
-  # ATTRIBUTE_TYPES
-  # a hash that describes the type of each of the model's fields.
-  #
-  # Each different type represents an Administrate::Field object,
-  # which determines how the attribute is displayed
-  # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
     goals: Field::HasMany,
-    id: Field::String.with_options(searchable: false),
+    teams: Field::HasMany,
+    id: Field::String,
     match_week: Field::Number,
     date: Field::DateTime,
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
   }.freeze
 
-  # COLLECTION_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's index page.
-  #
-  # By default, it's limited to four items to reduce clutter on index pages.
-  # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    "goals",
-    "id",
+    :match_week,
+    :teams,
+    :goals,
   ].freeze
 
-  # SHOW_PAGE_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
-    "goals",
-    "id",
-    "match_week",
-    "date",
-    "created_at",
-    "updated_at",
+    :goals,
+    :id,
+    :match_week,
+    :date,
+    :created_at,
+    :updated_at,
   ].freeze
 
-  # FORM_ATTRIBUTES
-  # an array of attributes that will be displayed
-  # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
-    "goals",
-    "match_week",
-    "date",
+    :goals,
+    :match_week,
+    :date,
   ].freeze
 
-  # COLLECTION_FILTERS
-  # a hash that defines filters that can be used while searching via the search
-  # field of the dashboard.
-  #
-  # For example to add an option to search for open resources by typing "open:"
-  # in the search field:
-  #
-  #   COLLECTION_FILTERS = {
-  #     open: ->(resources) { resources.where(open: true) }
-  #   }.freeze
   COLLECTION_FILTERS = {}.freeze
-
-  # Overwrite this method to customize how games are displayed
-  # across all pages of the admin dashboard.
-  #
-  # def display_resource(game)
-  #   "Game ##{game.id}"
-  # end
 end
