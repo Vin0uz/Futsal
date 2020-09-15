1f650b74d841b6a34c1b32115a111a3e3d6f7b25
Kevin Vinhas <kevinvinhas@gmail.com>
2020-09-15 11:51:29 +0200

    Bumping gems and symbolizing/cleaning dashboards of admin

--- app/dashboards/goal_dashboard.rb
+++ app/dashboards/goal_dashboard.rb
@@ -1,15 +1,10 @@
 require "administrate/base_dashboard"
 
 class GoalDashboard < Administrate::BaseDashboard
-  # ATTRIBUTE_TYPES
-  # a hash that describes the type of each of the model's fields.
-  #
-  # Each different type represents an Administrate::Field object,
-  # which determines how the attribute is displayed
-  # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
     scorer: Field::BelongsTo.with_options(class_name: "Player"),
     passer: Field::BelongsTo.with_options(class_name: "Player"),
+    team: Field::BelongsTo,
     game: Field::BelongsTo,
     id: Field::String.with_options(searchable: false),
     scorer_id: Field::String.with_options(searchable: false),
@@ -19,60 +14,33 @@ class GoalDashboard < Administrate::BaseDashboard
     updated_at: Field::DateTime,
   }.freeze
 
-  # COLLECTION_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's index page.
-  #
-  # By default, it's limited to four items to reduce clutter on index pages.
-  # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    "scorer",
-    "passer",
-    "game",
-    "id",
+    :game,
+    :scorer,
+    :passer,
+    :team,
   ].freeze
 
-  # SHOW_PAGE_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
-    "scorer",
-    "passer",
-    "game",
-    "id",
-    "scorer_id",
-    "passer_id",
-    "action_type",
-    "created_at",
-    "updated_at",
+    :game,
+    :scorer,
+    :passer,
+    :action_type,
+    :created_at,
+    :updated_at,
   ].freeze
 
-  # FORM_ATTRIBUTES
-  # an array of attributes that will be displayed
-  # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
-    "scorer",
-    "passer",
-    "game",
-    "scorer_id",
-    "passer_id",
-    "action_type",
+    :scorer,
+    :passer,
+    :game,
+    :team,
+    :action_type,
   ].freeze
 
-  # COLLECTION_FILTERS
-  # a hash that defines filters that can be used while searching via the search
-  # field of the dashboard.
-  #
-  # For example to add an option to search for open resources by typing "open:"
-  # in the search field:
-  #
-  #   COLLECTION_FILTERS = {
-  #     open: ->(resources) { resources.where(open: true) }
-  #   }.freeze
   COLLECTION_FILTERS = {}.freeze
 
-  # Overwrite this method to customize how goals are displayed
-  # across all pages of the admin dashboard.
-  #
-  # def display_resource(goal)
-  #   "Goal ##{goal.id}"
-  # end
+  def display_resource(goal)
+    "#{goal.game.match_week} - #{goal.team.acronym} - #{goal.scorer.name}"
+  end
 end
--- app/dashboards/team_dashboard.rb
+++ app/dashboards/team_dashboard.rb
@@ -1,16 +1,9 @@
 require "administrate/base_dashboard"
 
 class TeamDashboard < Administrate::BaseDashboard
-  # ATTRIBUTE_TYPES
-  # a hash that describes the type of each of the model's fields.
-  #
-  # Each different type represents an Administrate::Field object,
-  # which determines how the attribute is displayed
-  # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
     players: Field::HasMany,
-    home_games: Field::HasMany.with_options(class_name: "Game"),
-    away_games: Field::HasMany.with_options(class_name: "Game"),
+    games: Field::HasMany,
     id: Field::String.with_options(searchable: false),
     name: Field::String,
     acronym: Field::String,
@@ -18,58 +11,29 @@ class TeamDashboard < Administrate::BaseDashboard
     updated_at: Field::DateTime,
   }.freeze
 
-  # COLLECTION_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's index page.
-  #
-  # By default, it's limited to four items to reduce clutter on index pages.
-  # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    "players",
-    "home_games",
-    "away_games",
-    "id",
+    :name,
+    :acronym,
   ].freeze
 
-  # SHOW_PAGE_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
-    "players",
-    "home_games",
-    "away_games",
-    "id",
-    "name",
-    "acronym",
-    "created_at",
-    "updated_at",
+    :name,
+    :acronym,
+    :players,
+    :games,
+    :id,
+    :created_at,
+    :updated_at,
   ].freeze
 
-  # FORM_ATTRIBUTES
-  # an array of attributes that will be displayed
-  # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
-    "players",
-    "home_games",
-    "away_games",
-    "name",
-    "acronym",
+    :name,
+    :acronym,
   ].freeze
 
-  # COLLECTION_FILTERS
-  # a hash that defines filters that can be used while searching via the search
-  # field of the dashboard.
-  #
-  # For example to add an option to search for open resources by typing "open:"
-  # in the search field:
-  #
-  #   COLLECTION_FILTERS = {
-  #     open: ->(resources) { resources.where(open: true) }
-  #   }.freeze
   COLLECTION_FILTERS = {}.freeze
 
-  # Overwrite this method to customize how teams are displayed
-  # across all pages of the admin dashboard.
-  #
-  # def display_resource(team)
-  #   "Team ##{team.id}"
-  # end
+  def display_resource(team)
+    team.acronym
+  end
 end
